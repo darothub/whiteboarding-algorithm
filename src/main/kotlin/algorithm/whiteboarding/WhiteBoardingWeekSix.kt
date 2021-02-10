17769298970000d0849ce5dc9277843a999089e2
darot <darotudeen@gmail.com>
2021-02-10 11:54:15 +0100

    break instruction lines

--- src/main/kotlin/algorithm/whiteboarding/WhiteBoardingWeekSix.kt
+++ src/main/kotlin/algorithm/whiteboarding/WhiteBoardingWeekSix.kt
@@ -6,7 +6,9 @@ import java.util.*
 class WhiteBoardingWeekSix {
 
     companion object {
-//        An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+//        An array A consisting of N different integers is given.
+//        The array contains integers in the range [1..(N + 1)],
+//        which means that exactly one element is missing.
 //
 //        Your goal is to find that missing element.
 //
