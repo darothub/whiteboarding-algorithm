3f6985fe834b94ebdd5c69f1941cb244ce3c946e
Olamicode <oafagbemi@gmail.com>
2021-03-10 00:19:37 +0100

    week ten whiteboarding challenge done

--- src/main/kotlin/algorithm/main.kt
+++ src/main/kotlin/algorithm/main.kt
@@ -4,33 +4,13 @@ package algorithm
 import algorithm.whiteboarding.BinaryNode
 import algorithm.whiteboarding.WhiteBoardingWeekEight
 import algorithm.whiteboarding.binaryTreeHeight
+import algorithm.whiteboarding.lengthOfLongestSubstring
 
 
 fun main(){
 
 
-
-    val zero = BinaryNode(0)
-    val one = BinaryNode(1)
-    val five = BinaryNode(5)
-    val seven = BinaryNode(7)
-    val eight = BinaryNode(8)
-    val nine = BinaryNode(9)
-
-
-    seven.leftChild = one
-    one.leftChild = zero
-    one.rightChild = five
-    seven.rightChild = nine
-    nine.leftChild = eight
-
-
-    val tree = seven
-
-    val height = binaryTreeHeight(tree)
-
-    println(height)
-
+   lengthOfLongestSubstring("codingisawesome") // Answer 7
 
 
 }
